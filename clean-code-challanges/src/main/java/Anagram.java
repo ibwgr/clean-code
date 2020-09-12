8a95c3625f9e88450f99f70dc559db43983fc8da
Ueli Kunz <kunz@ideadapt.net>
2020-09-12 12:05:46 +0200

    add exercise introductions that describe the problem.

--- clean-code-challanges/src/main/java/Anagram.java
+++ clean-code-challanges/src/main/java/Anagram.java
@@ -1,5 +1,10 @@
 import java.util.List;
 
+/**
+ * Given a word and a list of possible anagrams, select the correct sublist.
+ *
+ * Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+ */
 public class Anagram {
 
   public Anagram(String word) {
