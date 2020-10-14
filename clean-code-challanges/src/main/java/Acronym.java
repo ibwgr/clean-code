56ee9f6113a0e892a3019b14bf1d390a7a5199f0
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2020-10-15 00:35:15 +0200

    Implemented a solution for the acronym challange

--- clean-code-challanges/src/main/java/Acronym.java
+++ clean-code-challanges/src/main/java/Acronym.java
@@ -5,52 +5,21 @@
  *
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
-
 class Acronym {
 
-  private String phrase;
+  private final String phrase;
 
   Acronym(String phrase) {
-    this.phrase = phrase;
+        this.phrase = phrase;
   }
-
-
-
 
   String get() {
-    lineDeleter(phrase);
-
-//    if(phrase.contains(" - ")) {
-//      phrase = phrase.replace("-"," ");
-//      return phrase;
-//    }
-
-   // PhraseContainsLine AND leftAndRightIsWhiteSpace
-
-  //   phrase = phrase.replace(" - "," ");
-
-    String[] words = phrase.split(" ");
-    StringBuilder acronymPhrase = new StringBuilder();
-    for (String word : words) {
-      if (word.startsWith("_") && word.endsWith("_")) {
-        String acronymMinusUnderline = word.substring(1, word.length() - 1);
-        acronymPhrase.append(acronymMinusUnderline.substring(0, 1).toUpperCase());
-
-      } else if (word.contains("-")) {
-        word.replace(word, "\\s");
-
-      } else {
-        acronymPhrase.append(word.substring(0, 1).toUpperCase());
-      }
+    StringBuffer acronym = new StringBuffer();
+    String[] words = phrase.trim().split("[\\s-_]+");
+    for(String word : words) {
+      acronym.append(word.toUpperCase().charAt(0));
     }
-    return acronymPhrase.toString();
-  }
-
-  public String lineDeleter(String phrase){
-    String result = phrase = phrase.replace(" - "," ");
-    result = phrase.replaceAll("_", "");
-    result = phrase.replaceAll("-", " ");
-return result;
+    return acronym.toString();
   }
 
 }
