8437446919e47c985a6c110562cbc40fc55db5a7
MitjaPerko <89871456+MitjaPerko@users.noreply.github.com>
2022-09-13 22:02:04 +0200

    Finito angram All Test's Green.

--- clean-code-challanges/src/main/java/Anagram.java
+++ clean-code-challanges/src/main/java/Anagram.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 /**
  * Given a word and a list of possible anagrams, select the correct sublist.
@@ -6,12 +6,30 @@ import java.util.List;
  * Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
  */
 public class Anagram {
+  private final String word;
 
   public Anagram(String word) {
+    this.word = word.toLowerCase();
 
   }
 
   public List<String> match(List<String> candidates) {
-    return null;
+    String sortedWord = stringSortAlphabet(word);
+    List<String> result = new ArrayList<String>();
+    for (String text:candidates) {
+      if (!word.equals(text.toLowerCase())){
+        if (sortedWord.equals(stringSortAlphabet(text.toLowerCase()))){
+          result.add(text);
+        }
+      }
+    }
+    return result;
   }
+
+  private String stringSortAlphabet(String word){
+    char[] stringToChar = word.toCharArray();
+    Arrays.sort(stringToChar);
+    return new String(stringToChar);
+  }
+
 }
